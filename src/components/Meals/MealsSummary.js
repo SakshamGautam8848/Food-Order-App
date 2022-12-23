3c62eec2c2545f13507dca2050365f56c92b1ca0
Thul DAi <sakshamgautam8848@gmail.com>
2022-12-23 19:48:39 +0545

    initial commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from "./MealsSummary.module.css";
+
+function MealsSummary() {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Come and Get it</h2>
+      <p>
+        Choose your favorite meal from our broad selecuintion of available meals
+        and enjoy a nice lunch and dinner at home with your friends or family.
+      </p>
+      <p>
+        All out meals are cooked with high-quality ingredients, just-in-time and
+        by experienced chefs!
+      </p>
+    </section>
+  );
+}
+
+export default MealsSummary;
